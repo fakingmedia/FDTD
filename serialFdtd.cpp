9896d1774a627c83969c8f98754aa792cb448491
fakingmedia <79916355+fakingmedia@users.noreply.github.com>
2024-04-01 12:19:56 +0800

    4、1

--- serialFdtd.cpp
+++ serialFdtd.cpp
@@ -6,14 +6,27 @@
 
 serialFdtd::serialFdtd() {
 
+    eta = deltaT * c * sqrt(1/pow(deltaL, 2) + 1/pow(deltaL, 2) + 1/pow(deltaL, 2));
+
+    n = 0;
 }
 
 bool serialFdtd::isStable() {
-    return deltaT * c * sqrt(1/pow(deltaX, 2) + 1/pow(deltaY, 2) + 1/pow(deltaZ, 2)) <= 1;
+    return eta <= 1;
 }
 
-void serialFdtd::updateE() {
 
+
+void serialFdtd::updateE() {
+    for (int i = 0; i < numX; i++)
+        for (int j = 0; j < numY; j++)
+            for (int k = 0; k < numZ; k++) {
+                ExPrev = Ex;
+                EyPrev = Ey;
+                EzPrev = Ez;
+
+                Ex = ExPrev + ;
+            }
 }
 
 void serialFdtd::updateH() {
--- serialFdtd.h
+++ serialFdtd.h
@@ -5,8 +5,8 @@
 #ifndef FDTD_SERIALFDTD_H
 #define FDTD_SERIALFDTD_H
 
-#include<cmath>
-#include<vector>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
@@ -14,19 +14,42 @@ class serialFdtd {
 protected:
     const double c = 300000000;
 
-    vector<vector<vector<vector<double>>>> E; //电场强度(t,x,y,z)
-    vector<vector<vector<vector<double>>>> H; //磁场强度(t,x,y,z)
+    vector<vector<vector<double>>> Ex; //电场强度(t,x,y,z)
+    vector<vector<vector<double>>> Ey;
+    vector<vector<vector<double>>> Ez;
+
+    vector<vector<vector<double>>> ExPrev; //电场强度(t,x,y,z)
+    vector<vector<vector<double>>> EyPrev;
+    vector<vector<vector<double>>> EzPrev;
+
+    vector<vector<vector<double>>> Hx; //磁场强度(t,x,y,z)
+    vector<vector<vector<double>>> Hy;
+    vector<vector<vector<double>>> Hz;
+
+    vector<vector<vector<double>>> HxPrev; //磁场强度(t,x,y,z)
+    vector<vector<vector<double>>> HyPrev;
+    vector<vector<vector<double>>> HzPrev;
+
     vector<vector<vector<vector<double>>>> J; //电流密度(t,x,y,z)
     vector<vector<vector<vector<double>>>> M; //磁流密度(t,x,y,z)
 
+    double Vx, Vy, Vz; //仿真空间大小
+    int maxT; //迭代次数上限
+
     double epsilon; //媒介的介电常数
     double mju; //媒介的磁导率
-    double sigmaE; //电导率
-    double sigmaM; //磁导率
+    //double sigmaE; //电导率
+    //double sigmaM; //磁导率
+
+    double deltaT; //时间步长
+    double deltaL; //x,y,z轴上的空间距离
+
+    double Sc; //稳定常数
+    double eta; //特性阻抗
 
     int n; //迭代次数
-    double deltaT; //时间步长
-    double deltaX, deltaY, deltaZ; //x,y,z轴上的空间距离
+    int numX, numY, numZ; //x, y, z轴的检测点数量
+
 public:
     serialFdtd();
 
@@ -37,6 +60,5 @@ public:
 
     void show();
 };
-
 
 #endif //FDTD_SERIALFDTD_H
