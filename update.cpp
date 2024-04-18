ae85e45d2ef88a5f9e7c42a41ff19aec37d35e84
fakingmedia <2531059809@qq.com>
2024-04-18 14:31:56 +0800

    updateE完成

--- update.cpp
+++ update.cpp
@@ -4,7 +4,49 @@
 #include <iostream>
 
 void Grid::updateE() {
+    int mm, nn, pp;
 
+    if (type == oneGrid) {
+        for (mm = 1; mm < sizeX - 1; mm++)
+            Ez1(mm) = Ceze1(mm) * Ez1(mm) + Cezh1(mm) * (Hy1(mm) - Hy1(mm - 1));
+
+    } else if (type == tmZGrid) {
+        for (mm = 1; mm < sizeX - 1; mm++)
+            for (nn = 1; nn < sizeY - 1; nn++)
+                Ez2(mm, nn) = Ceze2(mm, nn) * Ez2(mm, nn) +
+                        Cezh2 (mm, nn) * ((Hy2(mm, nn) - Hy2(mm - 1, nn)) - (Hx2 (mm, nn) - Hx2 (mm, nn - 1)));
+
+    } else if (type == teZGrid) {
+        for(mm = 1; mm < sizeX - 1; mm++)
+            for(nn = 1; nn < sizeY - 1; nn++)
+                Ex2 (mm, nn) = Cexe2(mm, nn) * Ex2(mm, nn) + Cexh2(mm, nn) * (Hz2(mm, nn) - Hz2(mm, nn - 1));
+
+        for(mm = 1; mm < sizeX - 1; mm++)
+            for(nn = 1; nn < sizeY - 1; nn++)
+                Ey2 (mm, nn) = Ceye2(mm, nn) * Ey2(mm, nn) - Ceyh2 (mm, nn) * (Hz2(mm, nn) - Hz2(mm - 1, nn));
+
+    } else if (type == threeGrid) {
+        for (mm = 0; mm < sizeX - 1; mm++)
+            for (nn = 1; nn < sizeY - 1; nn++)
+                for (pp = 1; pp < sizeZ - 1; pp++)
+                    Ex(mm, nn, pp) = Cexe(mm, nn, pp) * Ex(mm, nn, pp) + Cexh(mm, nn, pp) *
+                            ((Hz(mm, nn, pp) - Hz(mm, nn - 1, pp)) - (Hy(mm, nn, pp) - Hy(mm, nn, pp - 1)));
+
+        for (mm = 1; mm < sizeX - 1; mm++)
+            for (nn = 0; nn < sizeY - 1; nn++)
+                for (pp = 1; pp < sizeZ - 1; pp++)
+                    Ey(mm, nn, pp) = Ceye(mm, nn, pp) * Ey(mm, nn, pp) + Ceyh(mm, nn, pp) *
+                            ((Hx(mm, nn, pp) - Hx(mm, nn, pp - 1)) - (Hz(mm, nn, pp) - Hz(mm - 1, nn, pp)));
+
+        for (mm = 1; mm < sizeX - 1; mm++)
+            for (nn = 1; nn < sizeY - 1; nn++)
+                for (pp = 0; pp < sizeZ - 1; pp++)
+                    Ez(mm, nn, pp) = Ceze(mm, nn, pp) * Ez(mm, nn, pp) + Cezh(mm, nn, pp) *
+                            ((Hy(mm, nn, pp) - Hy(mm - 1, nn, pp)) - (Hx(mm, nn, pp) - Hx(mm, nn - 1, pp)));
+
+    } else {
+        std::cerr << "updateE: Unknown grid type. Terminating..." << std::endl;
+    }
 }
 
 void Grid::updateH() {
